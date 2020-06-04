1de6283efc413e1749fde5cf814f10162c2dbfe5
DhruvAgrawal16 <dhruva@iitbhilai.ac.in>
2020-06-04 18:24:10 +0530

    end of module 14

--- DatingApp-SPA/src/app/members/member-list/member-list.component.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.component.ts
@@ -13,7 +13,10 @@ import { PaginatedResult } from 'src/app/_models/pagination';
 export class MemberListComponent implements OnInit {
 users: User[];
 page = 1;
-itemsPerPage = 5;
+itemsPerPage = 3;
+user: User = JSON.parse(localStorage.getItem('userForListDto'));
+genderList = [{value: 'male', display: 'MALE'}, {value: 'female', display: 'FEMALE'}];
+userParams: any = {};
 pagination: Pagination;
 
 
@@ -22,7 +25,18 @@ pagination: Pagination;
 
   ngOnInit() {
     this.loadUsers();
-  }
+    this.userParams.gender = this.user.gender === 'male' ? 'female' : 'male';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+   }
+
+   resetFilter(){
+    this.userParams.gender = this.user.gender === 'male' ? 'female' : 'male';
+    this.userParams.minAge = 18;
+    this.userParams.maxAge = 99;
+    this.loadUsers2();
+   }
+
   pageChanged(event: any): void {
     this.pagination.currentPage = event.page;
     this.loadUsers2();
@@ -36,7 +50,8 @@ pagination: Pagination;
   });
 }
 loadUsers2(){
-  this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage).subscribe((users: PaginatedResult<User[]>) => {
+  this.userService.getUsers(this.pagination.currentPage, this.pagination.itemsPerPage, this.userParams)
+  .subscribe((users: PaginatedResult<User[]>) => {
   this.users = users.result;
   this.pagination = users.pagination; }
   , error => {
--- DatingApp.ApI/Helpers/UserParams.cs
+++ DatingApp.ApI/Helpers/UserParams.cs
@@ -10,6 +10,9 @@ namespace DatingApp.ApI.Helpers
             get { return pageSize; }
             set { pageSize = ((value > MaxPageSize) ? MaxPageSize : value ); }
         }
-        
+        public int UserId { get; set; }
+        public string Gender { get; set; }
+        public int MaxAge { get; set; } = 99;
+        public int MinAge { get; set; } = 18;
     }
 }
