76f31786cda968b52846da4c73537b21b5282a14
DhruvAgrawal16 <dhruva@iitbhilai.ac.in>
2020-06-06 21:03:49 +0530

    end of module 17

--- DatingApp.ApI/Helpers/UserParams.cs
+++ DatingApp.ApI/Helpers/UserParams.cs
@@ -14,5 +14,7 @@ namespace DatingApp.ApI.Helpers
         public string Gender { get; set; }
         public int MaxAge { get; set; } = 99;
         public int MinAge { get; set; } = 18;
+        // public bool Likees { get; set; }=false;
+        // public bool Likers { get; set; } = false;
     }
 }
